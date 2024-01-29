e6ce07aae84cca4b0a3907b51b74a39a5ee8924e
Jsalway99 <123057653+Jsalway99@users.noreply.github.com>
2024-01-29 14:08:14 -0700

    week2

---
+++ Spam.js
@@ -0,0 +1,24 @@
+
+function setup()
+{
+    createCanvas(600,800)
+}
+
+function draw()
+{
+    background(30)
+    fill(255,255,255)
+    ellipse(300,400,200,100);
+    ellipse(300,400,150,80);
+    ellipse(250,380,90,60);
+    ellipse(330,380,90,60);
+    //
+    fill(255,204,0)
+    circle(250,370,50,80)
+    fill(29,30,29)
+    fill(255,204,0)
+    circle(340,365,50,80,)
+    fill(255,192,203)
+    rect(270,415,70,31)
+    rect(230,425,70,31)
+}
